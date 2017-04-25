699a6f5e964186a3c6be721a83b0e0d03952ce06
Richard Carls <richard.j.carls@gmail.com>
2017-04-25 16:32:44 +0000

    Abandoning

--- indiana.js
+++ indiana.js
@@ -2,12 +2,14 @@ const winston = require('winston')
 const fs = require('mz/fs')
 const path = require('path')
 const yaml = require('js-yaml')
+const mongoDb = require('mongodb')
 
 const Koa = require('koa')
 const Router = require('koa-router')
 const handlebars = require('koa-handlebars')
-//const Markdown = require('markdown-it')
-//const matter = require('gray-matter')
+
+const Collection = require('./lib/collection')
+const contentHelper = require('./lib/helper-content')
 
 const defaultConfig = {
   collection: 'indiana',
@@ -56,49 +58,154 @@ config.handlebars.layoutPath = function layoutPath (id) {
 
   return id
 }
+config.handlebars.helpers = {
+  content: contentHelper(config.markdown)
+}
 
 indiana.use(require('koa-static')(config.staticDir))
 
 module.exports = indiana
 
-// Create router
-const router = new Router()
-router
-  .use(handlebars(config.handlebars))
-  .use(require('./lib/koa-materialized-path')({
-    matpath: {
-      key: 'matpath',
-      path: '__path',
-      name: '__name',
-      isdir: '__isdir'
-    }
-  }))
-  .use(require('./lib/koa-indiana-cache')({
-    mongoUrl: [
-      `mongodb://${process.env.MONGO_DOMAIN}`,
-      `:${process.env.MONGO_PORT}`,
-      `/${process.env.MONGO_DB}`
-    ].join(''),
-    collection: 'indiana',
-    matpath: {
-      key: 'matpath',
-      path: '__path',
-      name: '__name',
-      isdir: '__isdir'
-    },
-    root: config.contentDir,
-    logger: winston
-  }))
-  .get('*', async (ctx) => {
-    const view = /* ctx.state.view */'' || (ctx.state.__isdir ? 'default:index' : 'default:view')
+// Mongo
+const mongoUrl = [
+  `mongodb://${process.env.MONGO_DOMAIN}`,
+  `:${process.env.MONGO_PORT}`,
+  `/${process.env.MONGO_DB}`
+].join('')
 
-    const renderOpts = {
-      data: {
-        directory: ctx.indiana.siblings
-      }
-    }
+mongoDb.MongoClient.connect(mongoUrl)
+  .then(async (db) => {
+    indiana.context.db = db
 
-    await ctx.render(view, ctx.state, renderOpts)
+    // Drop collection if exists
+    // TODO: Get collection name before watcher init?
+    await db.collection('content').drop()
+      .then(() => winston.info('Dropped collection'))
+
+    // Collection
+    const collection = new Collection(config.contentDir, {
+      logger: winston
+    })
+
+    collection.on('add', (node, doc) => {
+      db.collection(collection.name)
+        .findOneAndUpdate(node, doc, {upsert: true })
+        .catch(err => winston.error(err))
+    })
+      .on('change', (node, doc) => {
+        db.collection(collection.name)
+          .findOneAndUpdate(node, doc, {upsert: true })
+          .catch(err => winston.error(err))
+      })
+      .on('unlink', (node) => {
+        db.collection(collection.name)
+          .findOneAndDelete(node)
+          .catch(err => winston.error(err))
+      })
+      .on('addIndex', (node, doc) => {
+        db.collection(collection.name)
+          .findOneAndUpdate(node, doc, {upsert: true })
+          .catch(err => winston.error(err))
+      })
+      .on('addDir', (node) => {
+        db.collection(collection.name)
+          .findOneAndUpdate(node, node, {upsert: true })
+          .catch(err => winston.error(err))
+      })
+      .on('unlinkDir', (node) => {
+        db.collection(collection.name)
+          .findOneAndDelete(node)
+          .catch(err => winston.error(err))
+      })
+
+    indiana.context.collection = collection
+
+    // Create router
+    const router = new Router()
+    router
+      .use(handlebars(config.handlebars))
+      .use(collection.middleware())
+      .use(async function nodeQuery (ctx, next) {
+        let result
+        try {
+          result = await ctx.db
+            .collection(collection.name)
+            .findOne(ctx.currentNode, {
+              sort: { isdir: 1 } // Prefer file over directory
+            })
+        } catch (e) {
+          winston.error('[indiana] Error retrieving node', e)
+          return ctx.throw(500)
+        }
+
+        if (!result) {
+          winston.warn('[indiana] Node not found', ctx.currentNode)
+          return ctx.throw(404, 'Content not found')
+        }
+
+        ctx.state = result
+
+        return next()
+      })
+      .use(async function directoryQuery (ctx, next) {
+        const { path, name, isdir } = ctx.currentNode
+
+        if (isdir) {
+          let query
+          let segs = path
+          if (name) {
+            segs = segs.concat([name])
+          }
+
+          // Construct Mongo matpath query on `path` array
+          query = segs.reduce((q, seg, index) => {
+            q[`path.${index}`] = seg
+            return q
+          }, { path: { $size: segs.length } })
+
+          try {
+            ctx.siblings = await ctx.db
+              .collection(collection.name)
+              .find(query)
+            // TODO: Allow custom sort
+              .sort({ isdir: -1, name: 1 }) // Directories on top
+              .toArray()
+          } catch (e) {
+            winston.error('Problem loading directory listing', e)
+          }
+        }
+
+        return next()
+      })
+      .use(async function rootQuery (ctx, next) {
+        try {
+          ctx.root = await ctx.db
+            .collection(collection.name)
+            .find({ 'path.0': '', path: { $size: 1 }})
+          // TODO: Allow custom sort
+            .sort({ isdir: -1, name: 1 })
+            .toArray()
+        } catch (e) {
+          winston.error('Problem loading root directory listing', e)
+        }
+
+        return next()
+      })
+      .get('*', async (ctx) => {
+        const view = ctx.state.view || /* TODO: view with same name */ (ctx.state.isdir ? 'default:index' : 'default:view')
+
+        const renderOpts = {
+          data: {
+            root: ctx.root,
+            directory: ctx.siblings
+          }
+        }
+
+        winston.debug('view', view)
+        winston.debug(ctx.path, ctx.state)
+
+        await ctx.render(view, ctx.state, renderOpts)
+      })
+
+    indiana.use(router.routes())
   })
-
-indiana.use(router.routes())
